86c18e7dd2d9fc7ad107ee93ba9f8514921ec4b0
truongdc21v7n559 <truongdc21v7n559@vlvh.ctu.edu.vn>
2025-10-01 09:12:16 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.routes");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Import router
-const contactsRouter = require("./app/routes/contact.routes");
-
-// Đăng ký router cho đường dẫn /api/contacts
 app.use("/api/contacts", contactsRouter);
 
 // Route mặc định
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
+});
+
+// handle 404 response
+app.use((req, res, next) => {
+    // không khớp route nào thì vào đây
+    next(new ApiError(404, "Resource not found"));
+});
+
+// error-handling middleware
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
 });
 
 module.exports = app;
